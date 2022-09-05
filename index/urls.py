e086fd42281fc163359b8d3a22ed007b25fb23f7
joedsoncarvalho <joedson.silva@laclaw.com.br>
2022-09-05 15:27:27 -0300

    Adicionando requisição com o Fetch

--- index/urls.py
+++ index/urls.py
@@ -4,7 +4,6 @@ from index.views import *
 urlpatterns = [
     path("", index, name='index'),
     path('funcionarios', funcionarios, name='funcionarios'),
-    path('produtos', produtos, name='produtos'),
     path('<int:produto_id>/', detalhando_produto, name='produto_detalhado'),
-    path('produtos2', exibindo_produtos, name='exibindo_produtos')
+    path('produtos', exibindo_produtos, name='exibindo_produtos')
 ]
